897b5df06363377d81e0723e09694095528b711d
Andy Preston <edgeeffect@gmail.com>
2021-06-01 17:52:05 +0100

    https://vuejs.org/v2/style-guide/

--- client/src/js/app/components/motd.vue
+++ client/src/js/app/components/motd.vue
@@ -1,9 +1,19 @@
 <template>
-    <notification-card v-if="!closed && motd && motd.message" :notification="motd">
-        <i slot="actions" class="fa fa-times" @click="closed = true"></i>
-    </notification-card>
-    <!-- Spacer for cases we have no message of the day -->
-    <div v-else class="tw-mb-4"></div>
+  <notification-card
+    v-if="!closed && motd && motd.message"
+    :notification="motd"
+  >
+    <i
+      slot="actions"
+      class="fa fa-times"
+      @click="closed = true"
+    />
+  </notification-card>
+  <!-- Spacer for cases we have no message of the day -->
+  <div
+    v-else
+    class="tw-mb-4"
+  />
 </template>
 
 <script>
