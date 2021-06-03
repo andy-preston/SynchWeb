dfa8b9b9d9fbca54eba4cdec0acb32c721bfedcf
Andy Preston <edgeeffect@gmail.com>
2021-06-03 13:41:23 +0100

    Split home page code between home.vue and new homepage-card.vue - added "Messages for Users"

---
+++ client/src/js/app/components/homepage-card.vue
@@ -0,0 +1,60 @@
+<template>
+  <context-colour-card
+    class="tw-flex-col tw-mb-4 tw-text-center tw-shadow"
+    :class="classesForThreeUp(threeUp)"
+    :level="level"
+  >
+    <p v-if="icon != ''">
+      <i
+        class="fa fa-4x"
+        :class="icon"
+      />
+    </p>
+    <p class="tw-text-2xl tw-py-2">
+      {{ title }}
+    </p>
+    <div class="tw-text-left tw-text-sm">
+      <p
+        v-for="message in messages"
+        class="tw-py-2"
+      >
+        {{ message }}
+      </p>
+    </div>
+  </context-colour-card>
+</template>
+
+<script>
+import ContextColourCard from './context-colour-card.vue'
+
+export default {
+    'name': 'HomepageCard',
+    'components': {
+        ContextColourCard,
+    },
+    props: {
+        'icon': {
+            'type': String,
+            'default': '',
+        },
+        'title': {
+            'type': String,
+            'required': true,
+        },
+        'messages': {
+            'type': Array,
+            'required': true,
+        },
+        'level': {
+            'type': String,
+            'default': '',
+        },
+        'threeUp': Boolean,
+    },
+    'methods': {
+        'classesForThreeUp': function(threeUp) {
+            return threeUp ? 'md:tw-m-4 tw-w-full md:tw-w-1/3' : ''
+        },
+    },
+}
+</script>
--- client/src/js/app/views/home.vue
+++ client/src/js/app/views/home.vue
@@ -1,52 +1,83 @@
 <template>
-    <section id="home">
-        <div class="tw-mx-auto">
-            <hero-title />
+  <section id="home">
+    <div class="tw-mx-auto">
+      <hero-title />
+      <div class="tw-flex tw-flex-col md:tw-flex-row tw-justify-between">
+        <homepage-card
+          three-up="true"
+          icon="fa-truck"
+          title="Prepare for Beamtime"
+          :messages="[
+            'With SynchWeb you can create Shipments and use integrated DHL shipping services to get your samples to site.',
+            'Your parcels or dewars are tracked across the facility so you can see where your samples are.',
+            'While registering samples, you can specify data collection recipes for automated collection and processing.',
+          ]"
+        />
+        <homepage-card
+          three-up="true"
+          icon="fa-clock-o"
+          title="Monitor Experiment"
+          :messages="[
+            'SynchWeb provides a live activity stream of events on beamlines.',
+            'You can observe the data collection process through webcam feeds of your session.',
+            'Results from automated and downstream processing pipelines are displayed for initial assessment.',
+          ]"
+        />
+        <homepage-card
+          three-up="true"
+          icon="fa-pie-chart"
+          title="Process and analyze data"
+          :messages="[
+            'SynchWeb includes analysis and visualisation views from DIALS, autoPROC, results from Jupyter notebooks etc.',
+            'Review electron density maps from Dimple and FastDP.',
+            'Reprocess data from single or multiple data collections.',
+          ]"
+        />
+      </div>
 
-            <div class="tw-flex tw-flex-col md:tw-flex-row tw-justify-between">
+      <div class="md:tw-mx-4">
+        <homepage-card
+          title="Messages for Users"
+          level="info"
+          :messages="[
+            'THIS IS JUST PLACEHOLDER TEXT - A RELEASE VERSION WOULD HAVE SOMETHING TO SAY HERE',
+            'Running SynchWeb requires setting up a Linux, Apache, MariaDB and PHP (LAMP) software stack. If running in production you should configure your Apache and PHP to serve secure pages only. The steps below describe how to build the software so it is ready to deploy onto your target server.',
+            'For development, a simple environment can be setup by using scripts provided here. They are not intended for production use but include scripts to automatically build and deploy the software on a local VM.',
+            'If you make use of code from this repository, please reference: Fisher et al., J. Appl. Cryst. (2015). 48, 927-932, doi:10.1107/S1600576715004847 https://journals.iucr.org/j/issues/2015/03/00/fs5101/index.html',
+          ]"
+        />
+      </div>
 
-                <div class="tw-flex-col tw-w-full md:tw-w-1/3 tw-mb-4 md:tw-m-4 tw-p-4 tw-border tw-border-gray-500 tw-bg-gray-300 tw-text-center tw-rounded tw-shadow">
-                    <p><i class="fa fa-4x fa-truck"/></p>
-                    <p class="tw-text-2xl tw-py-2">Prepare for Beamtime</p>
-                    <div class="tw-text-left tw-text-sm">
-                        <p class="tw-py-2">With SynchWeb you can create Shipments and use integrated DHL shipping services to get your samples to site.</p>
-                        <p class="tw-py-2">Your parcels or dewars are tracked across the facility so you can see where your samples are.</p>
-                        <p class="tw-py-2">While registering samples, you can specify data collection recipes for automated collection and processing.</p>
-                    </div>
-                </div>
-
-                <div class="tw-flex-col tw-w-full md:tw-w-1/3 tw-mb-4 md:tw-m-4 tw-p-4 tw-border tw-border-gray-500 tw-bg-gray-300 tw-text-center tw-rounded tw-shadow">
-                    <p><i class="fa fa-4x fa-clock-o"/></p>
-                    <p class="tw-text-2xl tw-p-2">Monitor Experiment</p>
-                    <div class="tw-text-left tw-text-sm">
-                        <p class="tw-py-2">SynchWeb provides a live activity stream of events on beamlines.</p>
-                        <p class="tw-py-2">You can observe the data collection process through webcam feeds of your session.</p>
-                        <p class="tw-py-2">Results from automated and downstream processing pipelines are displayed for initial assessment.</p>
-                    </div>
-                </div>
-
-                <div class="tw-flex-col tw-w-full md:tw-w-1/3 tw-mb-4 md:tw-m-4 tw-p-4 tw-border tw-border-gray-500 tw-bg-gray-300 tw-text-center tw-rounded tw-shadow">
-                    <p><i class="fa fa-4x fa-pie-chart"/></p>
-                    <p class="tw-text-2xl tw-p-2 ">Process and analyze data</p>
-                    <div class="tw-text-left tw-text-sm">
-                        <p class="tw-py-2">SynchWeb includes analysis and visualisation views from DIALS, autoPROC, results from Jupyter notebooks etc.</p>
-                        <p class="tw-py-2">Review electron density maps from Dimple and FastDP.</p>
-                        <p class="tw-py-2">Reprocess data from single or multiple data collections.</p>
-                    </div>
-                </div>
-
-            </div>
-
-            <div class="tw-flex tw-flex-col w-w-full tw-m-2 tw-p-4">
-                <p class="tw-text-center tw-text-lg tw-mb-4"><router-link to='/login' class="tw-underline">Login</router-link> to view your proposals and sessions</p>
-                <p v-if="dataCatalogue" class="tw-text-center tw-text-lg">If you are looking for archived data please visit the data catalogue <a :href="dataCatalogue.url" class="tw-underline">{{dataCatalogue.name}}</a></p>
-            </div>
-        </div>
-    </section>
+      <div class="tw-flex tw-flex-col w-w-full tw-m-2 tw-p-4">
+        <p class="tw-text-center tw-text-lg tw-mb-4">
+          <router-link
+            to="/login"
+            class="tw-underline"
+          >
+            Login
+          </router-link>
+          to view your proposals and sessions
+        </p>
+        <p
+          v-if="dataCatalogue"
+          class="tw-text-center tw-text-lg"
+        >
+          If you are looking for archived data please visit the data catalogue
+          <a
+            :href="dataCatalogue.url"
+            class="tw-underline"
+          >
+            {{ dataCatalogue.name }}
+          </a>
+        </p>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 import Hero from 'app/components/herotitle.vue'
+import HomepageCard from 'app/components/homepage-card.vue'
 import EventBus from 'app/components/utils/event-bus.js'
 import Config from 'config.json'
 
@@ -54,6 +85,7 @@ export default {
     name: 'Home',
     components: {
         'hero-title': Hero,
+        HomepageCard,
     },
     data() {
         return {
@@ -73,7 +105,7 @@ export default {
         }
     },
     created: function() {
-        let self = this
+        // let self = this
         // Reset proposal and associated type/model
         // We call the action so the store can handle the proposal type and model
         this.$store.dispatch('proposal/setProposal', null)
